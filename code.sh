2f4f65f706b51d62b2c8fe35060bf7d766d2a2a2
guyrenny <guy.renny@coralogix.com>
2023-07-09 14:33:19 +0300

    test

--- code.sh
+++ code.sh
@@ -1,5 +1,7 @@
-yq eval '.Metadata."AWS::CloudFormation::Interface".ParameterGroups[0].Parameters += "IntegrationId"' -i template.yaml
-yq eval --inplace '.Metadata."AWS::CloudFormation::Interface".ParameterLabels += {"IntegrationId": {"default": "Integration ID"}}' template.yaml
-yq eval --inplace '.Parameters += {"IntegrationId": {"Type": "String",  "Description": "The integration ID to register."}}' template.yaml
+#!/bin/bash
+file=$1
+yq eval '.Metadata."AWS::CloudFormation::Interface".ParameterGroups[0].Parameters += "IntegrationId"' -i $file
+yq eval --inplace '.Metadata."AWS::CloudFormation::Interface".ParameterLabels += {"IntegrationId": {"default": "Integration ID"}}' $file
+yq eval --inplace '.Parameters += {"IntegrationId": {"Type": "String",  "Description": "The integration ID to register."}}' $file
 
-cat s3-resource.txt  >> template.yaml
+cat s3-resource.txt  >> $file
