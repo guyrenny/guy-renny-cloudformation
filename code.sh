7ab8e4b47d89714a742fef77eb5466257c154f44
guyrenny <guy.renny@coralogix.com>
2023-07-09 15:26:07 +0300

    test

--- code.sh
+++ code.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 file=$1
+integration=$2
 yq eval '.Metadata."AWS::CloudFormation::Interface".ParameterGroups[0].Parameters += "IntegrationId"' -i $file
 yq eval --inplace '.Metadata."AWS::CloudFormation::Interface".ParameterLabels += {"IntegrationId": {"default": "Integration ID"}}' $file
 yq eval --inplace '.Parameters += {"IntegrationId": {"Type": "String",  "Description": "The integration ID to register."}}' $file
 
-cat s3-resource.txt  >> $file
+cat ${integration}-resource.txt  >> $file
