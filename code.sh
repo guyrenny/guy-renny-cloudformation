7995269130f04e5e324883d8d27e4b556a4284bd
guyrenny <guy.renny@coralogix.com>
2023-07-09 15:29:08 +0300

    test

--- code.sh
+++ code.sh
@@ -5,4 +5,4 @@ yq eval '.Metadata."AWS::CloudFormation::Interface".ParameterGroups[0].Parameter
 yq eval --inplace '.Metadata."AWS::CloudFormation::Interface".ParameterLabels += {"IntegrationId": {"default": "Integration ID"}}' $file
 yq eval --inplace '.Parameters += {"IntegrationId": {"Type": "String",  "Description": "The integration ID to register."}}' $file
 
-cat ${integration}-resource.txt  >> $file
+cat folder/${integration}-resource.txt  >> $file
